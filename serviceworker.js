bce80fbb1ab7a5cfd1b71749924cccb695c86d5b
Fhareza Alvindo <50841163+185410105@users.noreply.github.com>
2021-11-14 22:57:27 +0700

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -24,7 +24,7 @@ self.addEventListener("activate", (event) => {
       await Promise.all(
         cacheNames
           .filter((cacheName) => {})
-          .map((cacheName) => caches.delete(cacheName))
+          // .map((cacheName) => caches.delete(cacheName))
       );
     })()
   );
